a23cd44cfc5aaf1920e19cfc1efa5b98ec3ebd39
Wade James Russell <wados.russell70@gmail.com>
2023-12-16 15:09:02 +1100

    added game seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,18 +5,42 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void {
 
-         \App\Models\User::factory()->create([
-             'name' => 'Wade Russell',
-             'email' => 'wade@example.com',
-             'api_key' => 'APIKEYEXAMPLE'
+        \App\Models\User::factory()->create([
+            'name' => 'Wade Russell',
+            'email' => 'wade@example.com',
+            'api_key' => 'APIKEYEXAMPLE'
+        ]);
+
+        \App\Models\Game::factory()->create([
+            'name' => 'Uncharted: Drake\'s Fortune',
+            'user_id' => 1,
+        ]);
+
+        \App\Models\Game::factory()->create([
+            'name' => 'Uncharted 2: Among Thieves',
+            'user_id' => 1,
+        ]);
+
+        \App\Models\Game::factory()->create([
+            'name' => 'Uncharted 3: Drake\'s Deception',
+            'user_id' => 1,
+        ]);
+
+        \App\Models\Game::factory()->create([
+            'name' => 'Uncharted 4: A Thief\'s End',
+            'user_id' => 1,
+        ]);
+
+        \App\Models\Game::factory()->create([
+            'name' => 'Baldur\'s Gate 3',
+            'user_id' => 1,
         ]);
     }
 }
